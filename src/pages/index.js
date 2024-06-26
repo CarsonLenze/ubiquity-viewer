58a430cce3352ae67c2a4dca8e8fd47fc777e527
CarsonLenze <carson@overfuel.com>
2024-06-26 00:09:51 -0400

    data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,23 @@
+import { useEffect, useState } from "react";
+
 export default function Home() {
   return (
     <>
-    Hello World
+    <>Hello</>
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const url = 'https://static.ui.com/fingerprint/ui/public.json';
+  const res = await fetch(url);
+  const json = await res.json();
+
+  const filterd = json.devices.filter(x => x.sku === "UNAS-Pro");
+
+  return {
+    props: {
+        data: filterd
+    }
+  }
 }
